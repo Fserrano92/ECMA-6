3e83259d3f148c153cdd3ce55143254c5f9fa6b6
fabian serrano <fabian.serrano.92@gmail.com>
2021-12-10 09:25:05 -0600

    funcionalidades incluidas en Ecmascript7

---
+++ src/es7/index.js
@@ -0,0 +1,17 @@
+//--------------------- Metodo para arreglo -------------------------
+
+let number = [1, 2, 3, 4, 5, 7];
+
+if (number.includes(5)) {
+  console.log("Si se encuentra el valor");
+} else {
+  console.log("No se encuentra el valor");
+}
+
+//--------------------- Operadores para Exponetes -------------------------
+
+let base = 4;
+let exponent = 3;
+let result = base ** exponent;
+
+console.log(result);
